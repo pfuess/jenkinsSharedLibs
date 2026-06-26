a1a36bf50792f828c04f80d88f206e21d665fa0d
Peter Füss <peter.fuess@qnamic.com>
2026-06-26 15:19:36 +0200

    publishHtml integrated in qftest call

--- vars/qfTest.groovy
+++ vars/qfTest.groovy
@@ -1,14 +1,27 @@
 def call(Map config=[:]){
-    
-    bat """
-        qftestc.exe -batch -allowkilling -engine swt,awt ^
-        -runlog ${env.WORKSPACE}/_qftestRunLogs/${config.app}/qrz/Regression.qzp ^
-        -report.html ${env.WORKSPACE}/_qftestRunLogs/${config.app}/html ^
-        -report.junit ${env.WORKSPACE}/_qftestRunLogs/${config.app}/junit ^
-        -report.xml ${env.WORKSPACE}/_qftestRunLogs/${config.app}/xml ^
-        -report-thumbnails -report-scale-thumbnails 16 ^
-        ${config.vars} ^
-        -test \"${config.currentTest}\" ^
-        -exitcode-ignore-exception -run ${config.currentSuite}
-    """.replaceAll(/ {2,}/,' ')
+    try {
+        bat """
+            qftestc.exe -batch -allowkilling -engine swt,awt ^
+            -runlog ${env.WORKSPACE}/_qftestRunLogs/${config.app}/qrz/Regression.qzp ^
+            -report.html ${env.WORKSPACE}/_qftestRunLogs/${config.app}/html ^
+            -report.junit ${env.WORKSPACE}/_qftestRunLogs/${config.app}/junit ^
+            -report.xml ${env.WORKSPACE}/_qftestRunLogs/${config.app}/xml ^
+            -report-thumbnails -report-scale-thumbnails 16 ^
+            ${config.vars} ^
+            -test \"${config.currentTest}\" ^
+            -exitcode-ignore-exception -run ${config.currentSuite}
+        """.replaceAll(/ {2,}/,' ')
+    } finally {
+        publishHTML([
+            allowMissing          : false, 
+            alwaysLinkToLastBuild : false, 
+            keepAll               : true, 
+            reportDir             : "_qftestRunLogs/${config.app}/html",
+            reportFiles           : 'report.html', 
+            reportName            : "${config.app == 'RO' ? 'RailOpt' : config.app} Report", 
+            //reportTitles          : 'RailOpt Regression', 
+            useWrapperFileDirectly: true
+        ])
+    }
+
 }
